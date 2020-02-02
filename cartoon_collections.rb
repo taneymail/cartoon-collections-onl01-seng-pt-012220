fb52e820337543efcbe8a05f5810024e2cbb0d9a
TShipley <taneymail@gmail.com>
2020-02-02 03:12:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,6 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
 def summon_captain_planet(array)
   array.map do |a|
     a.capitalize << "!"
